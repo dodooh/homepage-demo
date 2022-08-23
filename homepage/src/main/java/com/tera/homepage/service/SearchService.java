e99443a280c36f4e6345e34fa704adadfc86bee7
nguyentranthanhnghia <nguyen2697s@gmail.com>
2022-08-23 16:29:08 +0700

    merge dev

--- homepage/src/main/java/com/tera/homepage/service/SearchService.java
+++ homepage/src/main/java/com/tera/homepage/service/SearchService.java
@@ -1,21 +1,7 @@
 package com.tera.homepage.service;
 
-import com.tera.homepage.model.Item;
 import com.tera.homepage.model.Media;
 import com.tera.homepage.model.MediaType;
-<<<<<<< HEAD
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
-import org.springframework.stereotype.Service;
-=======
->>>>>>> d75fb02 (update create + search)
 
 import java.util.List;
 
